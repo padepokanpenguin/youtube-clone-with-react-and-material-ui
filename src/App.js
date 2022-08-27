06e4f4cc749a6fc9351f2ec60064b06171359cb9
Aria Nur Jamal <jamal.psw@gmail.com>
2022-08-27 16:47:35 +0700

    adding rapid api key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Box from "@mui/material";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Box sx={{ backgroundColor: "#000" }}>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Feed />} />
           <Route path="/video/:id" element={<VideoDetail />} />
