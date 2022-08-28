2d1b4b57e3e9fc72955150066672bf66c37d8bb5
Aria Nur Jamal <jamal.psw@gmail.com>
2022-08-28 16:37:01 +0700

    finishing videoDetail component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,9 +3,16 @@ import { Box, Stack } from "@mui/material";
 import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
 
-export default function Videos({ videos }) {
+export default function Videos({ videos, direction }) {
+  if (!videos?.length) return "Loading...";
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos.map((video, idx) => (
         <Box key={idx}>
           {video.id.videoId && <VideoCard video={video} />}
